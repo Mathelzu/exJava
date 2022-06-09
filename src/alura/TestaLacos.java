c6b3b9277f3d21a76b55d0fb219e35542d846609
Matheus <matheusprogramao2003@gmail.com>
2022-06-09 16:24:29 -0400

    organiza TestaLacos

--- src/alura/TestaLacos.java
+++ src/alura/TestaLacos.java
@@ -2,8 +2,9 @@ package alura;
 
 public class TestaLacos {
     public static void main(String[] args) {
-        System.out.println("Tabuadinha feia:");
+        System.out.println("Tabuada:");
         for (int multiplicador = 1; multiplicador <= 10 ; multiplicador++){
+            System.out.print("Tabuada do " + multiplicador + " = " );
             for(int contador = 1; contador <= 10; contador++){
                 System.out.print( multiplicador * contador);
                 System.out.print(" ");
