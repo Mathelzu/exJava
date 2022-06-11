051d309fbdf2d1c6f06eb7594929d9ab6fa503e6
Matheus <matheusprogramao2003@gmail.com>
2022-06-11 02:03:41 -0400

    adiciona Fatorial e MultiploDe3

--- src/alura/Fatorial.java
+++ src/alura/Fatorial.java
@@ -2,9 +2,13 @@ package alura;
 
 public class Fatorial {
     public static void main(String[] args) {
+
         int fatorial = 1 ;
+
         for(int n = 1; n <= 10; n++){ fatorial *= n;
+
             System.out.println(fatorial);
+
         }
     }
 
--- src/alura/MultiploDe3.java
+++ src/alura/MultiploDe3.java
@@ -2,9 +2,13 @@ package alura;
 
 public class MultiploDe3 {
     public static void main(String[] args) {
+
         System.out.println("Multiplos de 3: ");
+
         for(int multiplo = 3; multiplo <= 100; multiplo ++){ if (multiplo % 3 == 0){
+
                 System.out.print( multiplo);
+
             System.out.print(" ");
             }
         }
